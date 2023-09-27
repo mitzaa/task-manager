8f098fa8c883a1bfdbb070a71203afc43573e55f
Marion <marionmillard@outlook.com>
2023-09-27 15:43:23 +1300

    changing taskId to taskID to correctly reflect DynamoDB table

--- backend/server.js
+++ backend/server.js
@@ -7,29 +7,37 @@ const app = express();
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-// AWS SDK Configuration
+// Configure AWS SDK
 AWS.config.update({
-  region: 'us-east-1'
+  region: 'us-east-1',
 });
+
+// Create a DynamoDB DocumentClient instance
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+// Define a route handler for the root URL ("/")
+app.get('/', (req, res) => {
+  res.send('Hello, Task Manager Backend'); // Replace with your desired response
+});
 
 // Add a task
 app.post('/tasks', (req, res) => {
-  const { taskId, taskName, taskDescription, status } = req.body;
+  const { taskID, taskName, taskDescription, status } = req.body;
 
   const params = {
     TableName: 'TaskManagerTasks',
     Item: {
-      taskId,
+      taskID,
       taskName,
       taskDescription,
-      status
-    }
+      status,
+    },
   };
 
   dynamoDb.put(params, (error) => {
     if (error) {
-      res.status(500).json({ error: "Unable to add task" });
+      console.error("Error adding task:", error); // Log the error for debugging
+      res.status(500).json({ error: "Unable to add task", details: error.message});
     } else {
       res.json({ success: true, message: "Task added successfully" });
     }
@@ -37,12 +45,12 @@ app.post('/tasks', (req, res) => {
 });
 
 // Retrieve a task by its ID
-app.get('/tasks/:taskId', (req, res) => {
+app.get('/tasks/:taskID', (req, res) => {
   const params = {
     TableName: 'TaskManagerTasks',
     Key: {
-      taskId: req.params.taskId
-    }
+      taskID: req.params.taskID,
+    },
   };
 
   dynamoDb.get(params, (error, result) => {
@@ -54,19 +62,34 @@ app.get('/tasks/:taskId', (req, res) => {
   });
 });
 
+// Retrieve all tasks
+app.get('/tasks', (req, res) => {
+  const params = {
+    TableName: 'TaskManagerTasks',
+  };
+
+  dynamoDb.scan(params, (error, result) => {
+    if (error) {
+      res.status(500).json({ error: "Unable to retrieve tasks" });
+    } else {
+      res.json(result.Items);
+    }
+  });
+});
+
 // Update a task's status
-app.patch('/tasks/:taskId', (req, res) => {
+app.patch('/tasks/:taskID', (req, res) => {
   const { status } = req.body;
 
   const params = {
     TableName: 'TaskManagerTasks',
     Key: {
-      taskId: req.params.taskId
+      taskID: req.params.taskID,
     },
     UpdateExpression: "set status = :s",
     ExpressionAttributeValues: {
-      ":s": status
-    }
+      ":s": status,
+    },
   };
 
   dynamoDb.update(params, (error) => {
@@ -79,12 +102,12 @@ app.patch('/tasks/:taskId', (req, res) => {
 });
 
 // Delete a task
-app.delete('/tasks/:taskId', (req, res) => {
+app.delete('/tasks/:taskID', (req, res) => {
   const params = {
     TableName: 'TaskManagerTasks',
     Key: {
-      taskId: req.params.taskId
-    }
+      taskID: req.params.taskID,
+    },
   };
 
   dynamoDb.delete(params, (error) => {
@@ -96,7 +119,16 @@ app.delete('/tasks/:taskId', (req, res) => {
   });
 });
 
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error("Error details:", err);
+
+  // Send an error response to the client
+  res.status(500).json({ error: "Internal Server Error", details: err.message });
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
